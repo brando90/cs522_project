c8ab26f2cafd55bfd461820a6e3ea14baa4c99b0
brando90 <brando90@mit.edu>
2018-12-18 14:50:48 -0600

    changed shared code of semantics to common place

--- project/Imp_Syntax.v
+++ project/Imp_Syntax.v
@@ -27,3 +27,47 @@ with Statement : Type :=
 
 Inductive Program : Type :=
   | Pgm : list string -> Statement -> Program.
+  
+  Fixpoint aeval (st : State) (a : AExp) : option nat :=
+  match a with
+  | ANum n => Some n
+  | AId x => st x
+  | APlus a1 a2 => match (aeval st a1) with
+    | Some n => match (aeval st a2) with
+      | Some n0 => Some (n + n0)
+      | None => None
+      end
+    | None => None
+    end
+  | ADiv a1 a2 => match (aeval st a2) with
+    | Some 0 => None
+    | Some (S n) => match (aeval st a1) with
+      | Some n0 => Some (div n0 (S n))
+      | None => None
+      end
+    | None => None
+    end
+  end.
+
+Fixpoint beval (st : State) (b : BExp) : option bool :=
+  match b with
+  | BVal v => Some v
+  | BLe a1 a2 => match (aeval st a1) with
+    | Some v => match (aeval st a2) with
+      | Some v0 => Some (leb v v0)
+      | None => None
+      end
+    | None => None
+    end
+  | BNot b1 => match (beval st b1) with
+    | Some v => Some (negb v)
+    | None => None
+    end
+  | BAnd b1 b2  => match (beval st b1) with
+    | Some v => match (beval st b2) with
+      | Some v0 => Some (andb v v0)
+      | None => None
+      end
+    | None => None
+    end
+  end.
